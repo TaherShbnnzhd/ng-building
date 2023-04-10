35746af8a954e02e5f29b71f1fd4a3a785bdac27
T.Shabannejad <T.Shabannejad@PC-SHABANNEJAD>
2023-04-10 11:16:11 +0330

    [04102023] | [dONE] | Add minor improvments to layout.

--- src/app/core/layout/loading-element/loading-element.component.ts
+++ src/app/core/layout/loading-element/loading-element.component.ts
@@ -1,10 +1,13 @@
 /* بِسْمِ اللهِ الرَّحْمنِ الرَّحِیم */
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'block-loading-element',
   templateUrl: './loading-element.component.html',
   styleUrls: ['./loading-element.component.scss'],
 })
-export class LoadingElementComponent {}
+export class LoadingElementComponent {
+  /** Loading element visibility. */
+  @Input() visible = false;
+}
